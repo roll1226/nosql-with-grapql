369ebca1a1e098f276b66d51ece4663d88a44647
roll1226 <tomo_yasu_4724@icloud.com>
2024-07-01 08:13:27 +0900

    fix[firebase]: express

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase-admin/app";
 initializeApp();
 
-import express, { Express } from "express";
+import express from "express";
 import { onRequest } from "firebase-functions/v2/https";
 import server from "./graphql";
 
-const app: Express = express();
+const app = express();
 
 server.start().then(() => {
   server.applyMiddleware({ app: app as never, path: "/" });
