7afced2aa9b628f3320531e0d9e0c3d912e50d10
roll1226 <tomo_yasu_4724@icloud.com>
2024-10-25 07:31:43 +0900

    update: emulator port consts

--- firebase/src/lib/FirebaseInitialize.ts
+++ firebase/src/lib/FirebaseInitialize.ts
@@ -3,6 +3,9 @@ import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 import { env } from "../env/dotEnv";
 
+const FIREBASE_EMULATOR_FIRESTORE_PORT = 8000;
+const FIREBASE_EMULATOR_FUNCTIONS_PORT = 5001;
+
 const isEmulator = () => {
   const useEmulator = env.isFirebaseEmulator();
   return !!(useEmulator && useEmulator);
@@ -14,8 +17,16 @@ const firebaseFunctions = getFunctions(app, "asia-northeast1");
 const firebaseFirestore = getFirestore(app);
 
 if (isEmulator()) {
-  connectFirestoreEmulator(firebaseFirestore, "localhost", 8000);
-  connectFunctionsEmulator(firebaseFunctions, "localhost", 5001);
+  connectFirestoreEmulator(
+    firebaseFirestore,
+    "localhost",
+    FIREBASE_EMULATOR_FIRESTORE_PORT
+  );
+  connectFunctionsEmulator(
+    firebaseFunctions,
+    "localhost",
+    FIREBASE_EMULATOR_FUNCTIONS_PORT
+  );
 }
 
 export { firebaseFirestore, firebaseFunctions };
